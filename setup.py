de38fb1f110bec3902e8276c71b0713ca26164d7
davila3 <david@texasfile.com>
2023-01-01 16:39:51 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colorPrint',
+      version='0.1',
+      description='Color Print is a very simple python package for printing with colors in most terminals. It uses escape codes to setup the colors for prints.',
+      url='https://github.com/davila3/colorPrint',
+      author='David Avila',
+      author_email='david.edward.avila+colorPrint@gmail.com',
+      license='MIT',
+      packages=['colorPrint'],
+      zip_safe=False)
